633155e15962eecd6a1fc0f135a691baf2868eca
chiakisaito <a.cop.p2p2@gmail.com>
2020-02-24 01:20:19 +0900

    じゃんけん課題訂正３〜相手の手

--- jankenpon_test.rb
+++ jankenpon_test.rb
@@ -32,22 +32,22 @@ enemy_hand = Enemy.new
 
 class Janken
 def janken_game(player_hand, enemy_hand)
-  rock_paper_scissors = {0 =>"ぐー", 1=>"チョキ", 2=>"パー"}
-  game_rezult = {0 =>"あいこ", 1 =>"勝ち", 2 =>"負け"}
+  rock_paper_scissors = {0 =>"グー", 1=>"チョキ", 2=>"パー"}
+  game_rezult = {0 =>"あいこ", 1 =>"負け", 2 =>"勝ち"}
   
   rezult = (player_hand - enemy_hand + 3) % 3
     if rezult == 1 || rezult == 2 #勝ちか負け
-      puts "相手の手は#{rock_paper_scissors[player_hand]}です。あなたの#{game_rezult[rezult]}です。"
+      puts "相手の手は#{rock_paper_scissors[enemy_hand]}です。あなたの#{game_rezult[rezult]}です。"
       return
     else
-      puts "相手の手は#{rock_paper_scissors[player_hand]}です。あなたの#{game_rezult[rezult]}です。"
+      puts "相手の手は#{rock_paper_scissors[enemy_hand]}です。あなたの#{game_rezult[rezult]}です。"
         player_hand = Player.new
         @player_hand =  player_hand.player
         enemy_hand = Enemy.new
         @enemy_hand = enemy_hand.enemy
         janken_game(@player_hand, @enemy_hand)
-        janken = Janken.new
-        janken.janken_game(@player_hand, @enemy_hand) 
+        #janken = Janken.new 削除
+        #janken.janken_game(@player_hand, @enemy_hand) 削除
     end
 end
 end
